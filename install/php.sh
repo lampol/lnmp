bba5d25c61a6658885c5577a22054060f2ae1b50
lampol <807968192@qq.com>
2017-10-09 15:41:20 +0800

    modified wget php

--- install/php.sh
+++ install/php.sh
@@ -20,7 +20,7 @@ function Mk_Cp_Php(){
 
 function Install_Php(){
 	[[ -f $SRC_DIR/$1 ]] && rm -rf $SRC_DIR/${1%%.*}*
-	wget http://am1.php.net/get/$1/from/this/mirror -P $SRC_DIR
+	wget http://am1.php.net/get/$1/from/this/mirror -O $SRC_DIR/$1
 	CONFIGURE=CONF_PHP
         Tar $1 && Install $CONFIGURE
         Mk_Cp_Php
