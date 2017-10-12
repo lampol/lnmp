16d03c6bfc07d68517609f68f78b7edd9afb820a
lampol <807968192@qq.com>
2017-10-12 11:21:00 +0800

    add php_memcache and extension

--- install/php_memcache.sh
+++ install/php_memcache.sh
@@ -8,16 +8,14 @@
 # Email             807968192@qq.com
 # Distributed under terms of the MIT license.
 #=============================================================
-function Mk_Cp_Mysql(){
-	chown -R mysql.mysql $SOFT_DIR/mysql
-	cp $SOFT_DIR/mysql/bin/mysql  /usr/bin
-	cp $INIT_DIR/mysqld  /etc/init.d/mysqld
-	mkdir $SOFT_DIR/mysql/etc
-	cp $CONF_DIR/my.cnf   $SOFT_DIR/mysql/etc/
-	chmod +x /etc/init.d/mysqld
-	rm -f /etc/my.cnf
-	$SOFT_DIR/mysql/scripts/mysql_install_db  --datadir=/usr/local/mysql/data  --basedir=/usr/local/mysql  --user=mysql
+function Mk_Cp_Php_Memcache(){
+        MEMCACHE_SO=$SOFT_DIR/php/lib/php/extensions/no-debug-non-zts-20121212/memcache.so
+        sed -i  's/extension=\/usr\/local\/php\/lib\/php\/extensions\/no-debug-non-zts-20121212\/memcache.so//g' $SOFT_DIR/php/etc/php.ini
+        sed -i "/Dynamic Extensions/aextension=$MEMCACHE_SO"   $SOFT_DIR/php/etc/php.ini
+        /etc/init.d/php-fpm  restart
+        php  -m 
 }
+
 
 function Install_Php_Memcached(){
         PHP_MEM=memcache-2.2.7.tar.gz
@@ -26,6 +24,6 @@ function Install_Php_Memcached(){
         [[ $? -ne 0 ]] && clear &&  echo -e "`Print_Color '31' "download $1 failed please try again"`" && exit 1
         CONFIGURE=CONF_MEMCACHE_PHP
         Tar_Phpize $PHP_MEM  && Install $CONFIGURE
-       # Mk_Cp_Mysql
+        Mk_Cp_Php_Memcache
 
 }
