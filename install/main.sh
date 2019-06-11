5dd828300eb37cdb7eff2de28efa89c69bfe0b87
root <root@localhost.localdomain>
2019-06-11 00:34:59 -0400

    修改php版本

--- install/main.sh
+++ install/main.sh
@@ -31,26 +31,20 @@ function Start_Install(){
         esac
         case "$PHP_VER" in
                 1)
-                 PHP_VER="php-5.3.29.tar.gz"
+                 PHP_VER="php-5.6.40.tar.gz"
                 ;;
                 2)
-                 PHP_VER="php-5.4.45.tar.gz"
+                 PHP_VER="php-7.0.33.tar.gz"
                  ;;
-		        3)
-		         PHP_VER="php-5.5.38.tar.gz"
-		        ;;
+		3)
+		 PHP_VER="php-7.1.30.tar.gz"
+		 ;;
                 4)
-                 PHP_VER="php-5.6.39.tar.gz"
+                 PHP_VER="php-7.2.19.tar.gz"
                  ;;
-		        5)
-  		        PHP_VER="php-7.0.33.tar.gz"
-  		        ;;
- 		        6)
- 		        PHP_VER="php-7.1.25.tar.gz"
- 		        ;;
- 		        7)
- 		        PHP_VER="php-7.2.13.tar.gz"
- 		        ;;
+		5)
+  		 PHP_VER="php-7.3.6.tar.gz"
+  		 ;;
         esac
 
         case "$MySQL_VER" in           
@@ -103,15 +97,13 @@ clear
         echo -e "`Print_Color '32' 'Please Select PHP Version '`"
         echo -e "================================================="
         PHP_VER=2
-        echo -e "1)  `Print_Color '32' 'php-5.3.29'`"
-        echo -e "2)  `Print_Color '32' 'php-5.4.45'`"
-        echo -e "3)  `Print_Color '32' 'php-5.5.38'`"
-        echo -e "4)  `Print_Color '32' 'php-5.6.39'`"
-	    echo -e "5)  `Print_Color '32' 'php-7.0.33'`"
-	    echo -e "6)  `Print_Color '32' 'php-7.1.25'`"
-        echo -e "7)  `Print_Color '32' 'php-7.2.13'`"
+        echo -e "1)  `Print_Color '32' 'php-5.6.40'`"
+        echo -e "2)  `Print_Color '32' 'php-7.0.33'`"
+        echo -e "3)  `Print_Color '32' 'php-7.1.30'`"
+        echo -e "4)  `Print_Color '32' 'php-7.2.19'`"
+	echo -e "5)  `Print_Color '32' 'php-7.3.6'`"
         read -p "Please Enter Your PHP Choice(1-7) Default(2):" PHP_VER
-        [[ $PHP_VER -lt 1 ]] || [[ $PHP_VER -ge 8 ]] && PHP_VER=2
+        [[ $PHP_VER -lt 1 ]] || [[ $PHP_VER -ge 6 ]] && PHP_VER=2
 #End Select PHP Ver
 clear
 #Start Select MySQL Ver
